d129f4842f7fd5eddc034343e3fdb256810a5db6
alexgaoyh <alexgaoyh@sina.com>
2017-10-08 16:17:19 +0800

    swaggerui部分注解参数修改

--- appclient/src/main/java/com/alexgaoyh/DiscoveryService.java
+++ appclient/src/main/java/com/alexgaoyh/DiscoveryService.java
@@ -16,7 +16,7 @@ public class DiscoveryService {
     private RestTemplate restTemplate;
 
     @GetMapping("/getServiceMethod")
-    @ApiOperation(value="demo-helloworld", notes="motes注解部分")
+    @ApiOperation(value="这个是接口方法的标题部分", notes="这个是接口方法的实现标注部分")
     public String getServiceMethod(){
         return  this.restTemplate.getForObject("http://APP-SERVICE/helloworld" , String.class);
     }
